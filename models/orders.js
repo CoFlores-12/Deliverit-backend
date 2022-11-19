a6165410de966120ff8dc470a953933a5af2ff7b
CoFlores-12 <obethflores2014@gmail.com>
2022-11-18 21:20:14 -0600

    session y ID new method

--- models/orders.js
+++ models/orders.js
@@ -1,10 +1,15 @@
 const mongoose  = require('mongoose')
 
 const schema = new mongoose.Schema({
+    id: String,
     status:    String,
     service: Number,
     total: Number,
     date: String,
+    payment: {
+        credit_card_number: String,
+        credit_card_name: String
+    },
     client:    {
         id: mongoose.SchemaTypes.ObjectId,
         name: String,
--- routes/client.js
+++ routes/client.js
@@ -3,26 +3,34 @@ const bp      = require('body-parser')
 const session = require('express-session')
 const cookieP = require("cookie-parser")
 const app     = express()
+const cors       = require('cors')
 const clientsSchema = require('../models/clients')
 const ordersSchema  = require('../models/orders')
-//const dealersSchema  = require('../models/dealers')
 const queries = require('../modules/queries')
+var ID = require("nodejs-unique-numeric-id-generator")
+
+
+app.use(
+    cors({
+      credentials: true,
+      origin: true,
+    }),
+  );
+  app.use(session({
+    secret: 'IS410',
+    saveUninitialized: true,
+    resave:false,
+    secure: false,
+    cookie:{
+        secure: false,
+        httpOnly: false
+    }
+}))
 
 app.use(bp.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieP())
-app.use(session({
-    secret: 'IS410',
-    resave: true,
-    saveUninitialized: true
-}))
 
-//TODO: remove this route
-app.get('/', (req, res) => {
-    queries.getFromDB(clientsSchema, {})
-        .then(results => res.send(results))
-        .catch(err => res.status(500).send(err))
-});
 
 app.post('/signin', async (req, res) => {
     if(!req.body.username || !req.body.email || !req.body.password){
@@ -45,7 +53,7 @@ app.post('/signin', async (req, res) => {
     queries.insertInToDB(clientsSchema, data)
         .then(result => {
             req.session.user = result  
-            res.send('signIn ' + result.id)
+            res.send(req.session.user)
         })
         .catch(err => {res.status(500).send(err)})
 })
@@ -63,7 +71,7 @@ app.post('/login', async (req, res) => {
 
     if (user[0].password == req.body.password) {
         req.session.user = user[0];
-        res.send('login ' + req.session.user.username);
+        res.send(req.session.user);
     }else{
         res.status(500).send("error credential")
     }
@@ -75,9 +83,12 @@ app.get('/logout', (req, res) => {
 })
 
 //TODO: Auth APIs
+app.get('/test', (req, res) => {
+    res.send(req.cookies.id)
+});
 
 app.get('/orders', async (req, res) => {
-    const orders = await ordersSchema.find({"client.id": req.session.user._id});
+    const orders = await ordersSchema.find({"client._id": req.cookies.id});
     res.send(JSON.stringify(orders))
 });
 
@@ -86,16 +97,20 @@ app.get('/orders/:idOrder', async (req, res) => {
     res.send(JSON.stringify(orders))
 });
 
-app.post('/CreateOrder', (req, res) => {
+app.post('/CreateOrder', async (req, res) => {
+    console.log(req.cookies.id);
+    const user = await clientsSchema.find({"_id": req.cookies.id});
     const data = {
+        id: ID.generate(new Date().toJSON()),
         status:  'Received',
         service: req.body.service,
         total: req.body.total,
         date: req.body.date,
+        payment: req.body.payment,
         client:    {
-            id: req.session.user._id,
-            name: req.session.user.username,
-            email: req.session.user.email
+            id: user._id,
+            name: user.username,
+            email: user.email
         },
         dealer:     {
             id: null,
@@ -106,8 +121,8 @@ app.post('/CreateOrder', (req, res) => {
         products:  req.body.products,
         locations: req.body.locations
     }
-    res.send(JSON.stringify(data));
     queries.insertInToDB(ordersSchema, data)
+    res.send(JSON.stringify(data));
 });
 
 module.exports = app
