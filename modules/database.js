298a2711f0661faa02b7796cb3433e947daab75b
CoFlores-12 <obethflores2014@gmail.com>
2022-12-26 19:22:00 -0600

    env

--- modules/database.js
+++ modules/database.js
@@ -2,6 +2,7 @@ const mongoose  =  require('mongoose')
 const db        = 'Deliverit'
 const port      = '27017'
 const host      = 'localhost'
+require('dotenv').config();
 
 class Database {
     constructor(){
@@ -9,7 +10,7 @@ class Database {
     }
 
     connect(){
-        mongoose.connect(`mongodb+srv://unah:is410@deliverit.elvsi2t.mongodb.net/?retryWrites=true&w=majority`)
+        mongoose.connect(`mongodb+srv://${process.env.USER_ID}:${process.env.USER_KEY}@deliverit.elvsi2t.mongodb.net/?retryWrites=true&w=majority`)
             .then(() => {console.log('connected to Database in mongoDB Atlas')})
             .catch(err => {console.log(err)})
     }
